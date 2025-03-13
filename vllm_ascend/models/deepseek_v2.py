045c113e010869bd0643e5f017eb0d4f0d846ce9
SidaoY <1024863041@qq.com>
2025-03-13 18:31:39 +0800

    Fix

--- vllm_ascend/models/deepseek_v2.py
+++ vllm_ascend/models/deepseek_v2.py
@@ -17,7 +17,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-from typing import Any, Dict, Iterable, Optional, Set, Tuple, Union
+from typing import Any, Dict, Iterable, List, Optional, Set, Tuple, Union
 
 import torch
 from torch import nn
